7fc34f8525dde74800c0f6ac4e69da6a84fbf173
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-10-26 17:42:16 -0500

    Proyecto Integrador - Listado de tareas-1, componentes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header';
+import Task from './components/Task';
+import TaskList from './components/TaskList';
+
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+      <TaskList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return(
+        <h1 style={{color:'#f74780'}}>Header</h1>
+    )
+}
+
+export default Header
